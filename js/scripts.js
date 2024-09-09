e38665d58d57b699614182343e623c6ba03d8f3a
smcre8tive <saramckeecre8tive@gmail.com>
2024-09-09 15:12:55 -0500

    Updates & Procrastigreat
    Updates to accessibility of color contrast and addition of Procrastigreat case study page.

--- js/scripts.js
+++ js/scripts.js
@@ -6,20 +6,6 @@ function menuToggle() {
       x.className = 'navtoggle';
     }
   }
-  $(document).ready(function() {
-    // Select all anchor links with href starting with '#'
-    $('a[href^="#"]').on('click', function(event) {
-  
-      // Prevent default anchor click behavior
-      event.preventDefault();
-  
-      // Store the hash in a variable
-      var target = this.hash;
-      var $target = $(target);
-  
-      // Smooth scroll to the target element
-      $('html, body').animate({
-        scrollTop: $target.offset().top
-      }, 800); // Adjust the duration (in milliseconds) as needed
-    });
-  });
+$('#ParentContainer').scroll(function() { 
+    $('#FixedDiv').css('top', $(this).scrollTop());
+});
